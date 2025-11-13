139cd2856ee9d4dc3e15b9bbceade408937c6382
sudipX <sudip-basnet@outlook.com>
2025-11-13 19:58:02 +0545

    add 281

---
+++ Practice_Problems/A_281.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462643383279502884
+#define debug(x) cerr << #x << ":" << x << endl;
+using namespace std;
+
+void solve() {
+    string s;cin>>s;
+    if(s[0]>=97)s[0]-=32;
+    cout<<s<<endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.precision(10);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
