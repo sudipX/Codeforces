b85810cbc23d449d5b3aeea324e046a0217d317d
sudipX <sudip-basnet@outlook.com>
2025-11-18 15:02:17 +0545

    add A 339

---
+++ Practice_Problems/A_339.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007
+#define PI 3.141592653589793238462643383279502884
+#define debug(x) cerr << #x << ":" << x << endl;
+using namespace std;
+
+void solve() {
+    string s;cin>>s;
+    sort(all(s));
+    int n=s.length();
+    for(int i=n/2;i<n;i++){
+        cout<<s[i];
+        if(i!=n-1)cout<<"+";
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.precision(10);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
