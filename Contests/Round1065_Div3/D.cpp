a54fb953d1df04a67bb2478f6f8923a90d28d6c9
sudipX <sudip-basnet@outlook.com>
2025-11-21 17:18:45 +0545

    add Round 1065 D

--- Contests/Round1065_Div3/D.cpp
+++ Contests/Round1065_Div3/D.cpp
@@ -8,14 +8,22 @@
 using namespace std;
 
 void solve() {
+    int n,bit,index;cin>>n;
+    vector<int>a(n),b(n);
+    int xorr=0;
+    for(int i=0;i<n;i++){cin>>a[i];xorr^=a[i];}
+    for(int i=0;i<n;i++){cin>>b[i];xorr^=b[i];}
+    if(xorr==0){cout<<"Tie\n";return;}
+    for(int i=0;i<20;i++)if(xorr& 1<<i)bit=i;
+    for(int i=0;i<n;i++)if((a[i]^b[i])&1<<bit)index=i;
+    cout<<(index&1?"Mai\n":"Ajisai\n");
 }
-
 int32_t main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.precision(10);
     int t = 1;
-    //cin >> t;
+    cin >> t;
     while (t--) {
         solve();
     }
